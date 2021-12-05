6bdb6104da127b64cece31ede6d1f3c3bc40e91a
Ellie074 <ellie074074@gmail.com>
2021-12-05 21:48:55 +0300

    add heroes mock

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import {Hero} from './hero';
+
+
+export const HEROES: Hero[] = [
+    {id: 11, name: 'Geralt'},
+    {id: 12, name: 'Shepard'},
+    {id: 13, name: 'Snake'},
+    {id: 14, name: 'Spider-Man'},
+    {id: 15, name: 'Batman'},
+    {id: 16, name: 'Joel'},
+    {id: 17, name: 'Sub-Zero'},
+    {id: 18, name: 'Doom'},
+    {id: 19, name: 'Levi'},
+    {id: 20, name: 'Victor'}
+];
